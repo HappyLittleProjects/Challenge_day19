2f74f624e13af9026b0a9fecf6200501be7c7611
HappyLittleProjects <lindalau81@gmail.com>
2022-09-21 15:45:44 -0700

    Initial Commit

---
+++ Challenge_day19/Challenge_day19App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_day19App.swift
+//  Challenge_day19
+//
+//  Created by Linda Lau on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_day19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge_day19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge_day19
+//
+//  Created by Linda Lau on 9/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
